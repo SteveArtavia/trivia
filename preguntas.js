7f7f1f68713fc49bd96f76235de540f32e6c989f
Steve Artavia <stvartavia@gmail.com>
2025-01-28 05:52:44 -0600

    preguntas agregadas a base de datos

---
+++ preguntas.js
@@ -0,0 +1,52 @@
+let preguntas = [
+    {
+        pregunta: '¿Cuál es la capital de Australia?',
+        opciones: ['Sydney', 'Melbourne', 'Canberra', 'Perth'],
+        respuestaCorrecta: 'Canberra'
+    },
+    {
+        pregunta: '¿Quién pintó la Mona Lisa?',
+        opciones: ['Vincent van Gogh', 'Leonardo da Vinci', 'Pablo Picasso', 'Michelangelo'],
+        respuestaCorrecta: 'Leonardo da Vinci'
+    },
+    {
+        pregunta: '¿Cuál es el animal más rápido del mundo?',
+        opciones: ['Leopardo', 'Guepardo', 'León', 'Tigre'],
+        respuestaCorrecta: 'Guepardo'
+    },
+    {
+        pregunta: '¿En qué año se descubrió América?',
+        opciones: ['1492', '1519', '1453', '1532'],
+        respuestaCorrecta: '1492'
+    },
+    {
+        pregunta: '¿Cuál es el elemento químico más abundante en el universo?',
+        opciones: ['Oxígeno', 'Hidrógeno', 'Carbono', 'Nitrógeno'],
+        respuestaCorrecta: 'Hidrógeno'
+    },
+    {
+        pregunta: '¿Cuál es el océano más grande de la Tierra?',
+        opciones: ['Océano Atlántico', 'Océano Índico', 'Océano Pacífico', 'Océano Ártico'],
+        respuestaCorrecta: 'Océano Pacífico'
+    },
+    {
+        pregunta: '¿Quién escribió la obra de teatro "Romeo y Julieta"?',
+        opciones: ['Miguel de Cervantes', 'William Shakespeare', 'Charles Dickens', 'Mark Twain'],
+        respuestaCorrecta: 'William Shakespeare'
+    },
+    {
+        pregunta: '¿Cuál es la fórmula química del agua?',
+        opciones: ['CO2', 'NaCl', 'H2O', 'O2'],
+        respuestaCorrecta: 'H2O'
+    },
+    {
+        pregunta: '¿En qué país se encuentra la Torre Eiffel?',
+        opciones: ['Londres', 'Nueva York', 'París', 'Roma'],
+        respuestaCorrecta: 'París'
+    },
+    {
+        pregunta: '¿Cuál es el planeta más cercano al Sol?',
+        opciones: ['Marte', 'Venus', 'Mercurio', 'Tierra'],
+        respuestaCorrecta: 'Mercurio'
+    }
+];
